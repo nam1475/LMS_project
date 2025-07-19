c4cdfbcee56b30ec752bab2a38444bc6c245fa3e
Nam <phanhainam20035@gmail.com>
2025-07-19 11:00:28 +0700

    Update deployment

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,18 @@ export default defineConfig({
                 'resources/js/admin/login.js',
                 'resources/js/admin/admin.js',
                 'resources/js/admin/course.js',
+                'resources/js/admin/notification.js',
 
                 'resources/js/frontend/course.js',
                 'resources/js/frontend/frontend.js',
                 'resources/js/frontend/player.js',
+                'resources/js/frontend/review.js',
+                'resources/js/frontend/comment.js',
+                'resources/js/frontend/chat.js',
+                'resources/js/frontend/notification.js',
+
+                'resources/js/global.js',
+                'resources/js/upload.js',
 
 
             ],
